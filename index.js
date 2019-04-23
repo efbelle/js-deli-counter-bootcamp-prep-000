09f328026fc0105368c643aedeb1e467d810e23c
peaceful-state-9936 <efua.mensahbrown@gmail.com>
2019-04-23 15:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var katzDeliLine = [];
+/* var katzDeliLine = [];
 function takeANumber (katzDeliLine,name){
  katzDeliLine.push(name);
  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  }*/
+  
+  let counter = 0; 
+  function takeANumber(katzDeliLine){
+    counter ++;
+    katzDeliLine.push(counter);
+    return (`${counter}`);
+    
   }
+  
+  
   
   function nowServing(katzDeliLine){
    if (katzDeliLine.length === 0) {
@@ -14,17 +24,17 @@ function takeANumber (katzDeliLine,name){
 }
 
 var katzDeliLine = [];
-function currentLine(name){
-  for (let i = 0; i < name.length; i++){
-   katzDeliLine.push(` `+[i+1]+`. `  + name[i]) 
+function currentLine(deliLine){
+  for (let i = 0; i < deliLine.length; i++){
+   katzDeliLine.push(` `+[i+1]+`. `  + deliLine[i]) 
   }
   
-  if (name.length === 0){
+  if (deliLine.length === 0){
    return "The line is currently empty.";
   
   }else
  
   return (`The line is currently:${katzDeliLine}`);
   
-
 }
+
